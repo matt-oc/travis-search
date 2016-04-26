a0df2626bfe91298afd0ae572f8962a26f677078
Matthew O'Connor <matthewocsecond@gmail.com>
2016-04-26 17:47:53 +0100

    srv file removed

--- srv/travis-dev.js
+++
@@ -1,6 +0,0 @@
-var STATS = process.env.STATS || 'localhost'
-require('seneca')()
-  .use('../travis.js')
-  .client({ host:'localhost', pin:{role:'travis', cmd:'*'}})
-  .listen(44006)
-  .repl(43005)
